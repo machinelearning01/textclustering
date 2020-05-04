cc7a9132b24935f36bee8f349c7b63301bea8918
jinraj <38273588+jinraj@users.noreply.github.com>
2020-05-04 11:35:02 +0530

    added flask

--- handler.py
+++ handler.py
@@ -16,6 +16,9 @@ if export_results_to_csv is set true, then save the results into excel file
 from validation import validate
 from input_data import input_data
 from file_mgmt import write_excel
+from flask import Flask, request, jsonify
+import pandas as pd
+app = Flask(__name__)
 
 #Input parameters
 params = {
@@ -31,22 +34,53 @@ params = {
         "max_utterances_similarity": 0.4,
         "min_utterances_similarity": 0.2
     },
-    "export_results_to_csv": False
+    "export_results_to_csv": True
 }
 
-print("validating...")
-resp = validate(params)
+def start(parameters):
+    msg =""
+    print("validating...")
+    resp = validate(parameters)
 
-if type(resp) == str:
-    print("alert:", resp)
-else:
-    print("initialised")
-    print("executing...")
-    intents = resp.execute()
-    if params["export_results_to_csv"]:
-        output_csv_filename = params.botname+'_output.csv'
-        write_excel(intents, output_csv_filename)
-        print("exported results to csv file - " + output_csv_filename)
-    print("completed")
+    if type(resp) == str:
+        print("alert:", resp)
+        msg = {"status":400, "body":resp}
+    else:
+        print("initialised")
+        print("executing...")
+        intents = resp.execute()
+        if parameters["export_results_to_csv"]:
+            output_csv_filename = 'csv_output.csv'
+            write_excel(intents, output_csv_filename)
+            msg = {"status": 200, "body": "results are saved to csv file - " + output_csv_filename}
+        else:
+            msg = {"status": 200, "body": "completed successfully"}
+    return msg
 
+
+@app.route('/upload_excel', methods=['POST'])
+def upload_file():
+    # I used form data type which means there is a
+    # "Content-Type: application/x-www-form-urlencoded"
+    # header in my request
+    raw_data = request.files['myfile']
+    data_xls = pd.read_excel(raw_data)
+    arr_data = data_xls["utterances"].tolist()
+
+    if len(arr_data) >=1:
+        params["excel_data"] = arr_data
+        response = start(params)
+        print(response)
+        return jsonify(response)
+    else:
+        return jsonify({"status":400, "body":"excel file is empty"})
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Automatic Utterances Clustering for Chatbots</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
